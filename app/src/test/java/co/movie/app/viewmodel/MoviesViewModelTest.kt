2864190f0acef5a3633a6ee4399eb480363b037a
Jaime Parra <jparra@picap.co>
2021-02-26 16:13:50 -0500

    dependencies compose

--- app/src/test/java/co/movie/app/viewmodel/MoviesViewModelTest.kt
+++ app/src/test/java/co/movie/app/viewmodel/MoviesViewModelTest.kt
@@ -1,6 +1,6 @@
 package co.movie.app.viewmodel
 
-import android.content.Context
+import co.movie.app.MainCoroutinesRule
 import co.movie.app.core.either.Either
 import co.movie.app.core.exception.Failure
 import co.movie.app.model.MoviesResults
@@ -8,9 +8,11 @@ import co.movie.app.usecase.GetLocalMoviesUseCase
 import co.movie.app.usecase.GetMoviesUseCase
 import co.movie.app.viewmodel.search.MoviesViewModel
 import co.movie.app.viewmodel.search.state.MoviesState
+import com.airbnb.mvrx.Success
 import com.airbnb.mvrx.test.MvRxTestRule
 import com.airbnb.mvrx.withState
 import com.nhaarman.mockitokotlin2.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
@@ -20,12 +22,12 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
 class MoviesViewModelTest {
 
     @get:Rule
     val mvrxRule = MvRxTestRule()
 
-    private val callback = mock<(Flow<Either<Failure, MoviesResults>>) -> Unit>()
     private val getMoviesUseCase: GetMoviesUseCase = mock()
     private val localMoviesUseCase: GetLocalMoviesUseCase = mock()
 
@@ -41,23 +43,17 @@ class MoviesViewModelTest {
     }
 
     @Test
-    fun searchMovies_ChangeStatusSuccess(): Unit = runBlocking{
-        val mockData = MoviesResults()
+    fun searchMovies_ChangeStatusSuccess(): Unit = runBlocking {
+        val captor = argumentCaptor<(Flow<Either<Failure, MoviesResults>>) -> Unit>()
 
-        whenever(getMoviesUseCase.run(params = 1)).doReturn(
-                flow {
-                    Either.Right(mockData)
-                }
-        )
-
-        val flow = verify(getMoviesUseCase).run(params = 1)
+        verify(getMoviesUseCase).run(params = 1)
 
         withState(viewModel) {
             assertEquals(it.isLoading, true)
         }
 
-        flow.collect { result: Either<Failure, MoviesResults> ->
-            assertTrue(result.isRight)
+        withState(viewModel) {
+            assertTrue(it.moviesState is Success)
         }
 
         withState(viewModel) {
